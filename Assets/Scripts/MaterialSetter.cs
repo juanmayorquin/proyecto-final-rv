785a0ec3d23cfed560a2018c1f13df0d31bb4769
juanmayorquin <127550805+juanmayorquin@users.noreply.github.com>
2024-05-30 03:37:03 -0500

    Se añade interacción con puertas, y transferencia de materiales

--- Assets/Scripts/MaterialSetter.cs
+++ Assets/Scripts/MaterialSetter.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MaterialSetter : MonoBehaviour
 {
+    [SerializeField] Material mat;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mat = GetComponent<MeshRenderer>().materials[0];
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag(this.gameObject.tag))
+        {
+            collision.gameObject.GetComponent<MeshRenderer>().material = mat;
+            
+        }
     }
 }
